3ceb85e39f0ff2f31f4e873c48e0710af1df9491
Michael Xie <michael@parkside.app>
2021-07-19 20:04:24 -0700

    aws-cli-oidc 0.2.0

--- Formula/aws-cli-oidc.rb
+++ Formula/aws-cli-oidc.rb
@@ -1,8 +1,8 @@
 class AwsCliOidc < Formula
   desc "Automatically start your go.mod Github dependencies to build aws-cli-oidc"
   homepage "https://github.com/parkside-securities/aws-cli-oidc"
-  url "https://github.com/parkside-securities/aws-cli-oidc/archive/refs/tags/0.1.1.tar.gz"
-  sha256 "0797b255a6e3d54ec89dbb41b541fcb523f005dcf4bee543d4452689bfe6c074"
+  url "https://github.com/parkside-securities/aws-cli-oidc/archive/refs/tags/0.2.0.tar.gz"
+  sha256 "204c24997c9f0271bf38e1826caa1e3bc7bc7fd0f92e3ca7af0b749e44cc9a68"
   license "MIT"
 
   depends_on "go" => :build
